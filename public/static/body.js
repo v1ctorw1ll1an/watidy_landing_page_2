9a45c1fc05d9c87cc8d61b6af3dcf8618798483e
V1ctorW1ll1an <victorwillian1012@gmail.com>
2026-03-30 15:09:26 -0300

    feat: add Google Tag Manager scripts and configure CORS for JavaScript files

---
+++ public/static/body.js
@@ -0,0 +1,4 @@
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.servertest.watidy.com.br/ns.html?id=GTM-NKDNR3WB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
